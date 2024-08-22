88be5b11e4f21672c1edd1369eab30f889881fd4
Avinash Nishad <avinash.2023ug2013@iiitranchi.ac.in>
2024-08-23 02:39:57 +0530

    3rd commit

--- src/Components/Container.jsx
+++ src/Components/Container.jsx
@@ -59,23 +59,23 @@ function Container() {
 
   return (
     
-    <div className="absolute rounded-[20px] flex flex-cols-3 md:flex-nowrap flex-wrap z-30  border-inherit bg-inherit min-w-60 md:h-screen   h-full  w-screen ">
+    <div className="absolute rounded-[20px] flex flex-cols-3 gap-12 justify-center md:flex-nowrap flex-wrap z-30  border-inherit bg-inherit min-w-60 md:h-screen   h-[100rem]  w-screen bg-green-400 sm:bg-transparent  ">
       <Cards 
         tempData={humidityData ? `${humidityData}%` : "Loading..."} // Pass humidity data to Cards component
         title="Humidity"
-        description="Real-time humidity data streamed directly from our IoT sensors, keeping you informed and in control, anytime, anywhere!"
+        description="Real-time humidity data streamed directly "
         url="https://imgs.search.brave.com/2ksGhWJmwxmPd5-zC4z-0QTbDvCW8lksqqWFpncFUmA/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9jZG4t/aWNvbnMtcG5nLmZy/ZWVwaWsuY29tLzI1/Ni8zNzUxLzM3NTEy/OTEucG5nP3NlbXQ9/YWlzX2h5YnJpZA"
       />
       <Cards
         tempData={tempData ? `${tempData} °C ` : "Loading..."} // Pass temp data to Cards component
         title="Temperature"
-        description="Real-time humidity data streamed directly from our IoT sensors, keeping you informed and in control, anytime, anywhere!"
+        description="Real-time Temperature data streamed directly "
         url="https://cdn-icons-png.flaticon.com/512/566/566675.png"
       />
       <Cards
         tempData={moistureData ? `${moistureData}%` : "Loading..."} // Pass moisture data to Cards component
         title="Plant Moisture"
-        description="Real-time humidity data streamed directly from our IoT sensors, keeping you informed and in control, anytime, anywhere!"
+        description="Real-time Plant Moisture data streamed directly "
         url="https://cdn-icons-png.flaticon.com/512/8809/8809827.png"
       />
     </div>
--- src/Components/background.jsx
+++ src/Components/background.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 function Background() {
   return (
-    <div className='absolute border z-[2] w-full h-full  bg-[url("/src/assets/green.png")]  object-fit flex justify-center ' >
-      <div className="flex justify-center items-center text-[5vw] absolute top-0 w-full py-6 font-semibold text-green-950">
+    <div className='absolute border z-[2] w-screen h-screen  bg-[url("/src/assets/green.png")]  object-fit flex justify-center ' >
+      <div className="flex justify-center items-center text-[60px] md:text-[80px] absolute top-0 w-full md:py-12 font-semibold text-green-950">
         Soil Details
       </div>
 
-      <h1 className="absolute m-[40vh] text-[12vw] leading-none tracking-tighter font-semibold ">
+      <h1 className="fixed self-center text-[90px] md:text-[250px] leading-none  font-semibold ">
         Gardener
       </h1>
     </div>
